fda2c95039caf667f0fceed3198a1f197d3661be
tai21042004 <tai21042004@gmail.com>
2025-11-11 21:43:49 +0700

    refactor: clean up configuration files and remove unused assets
    - Deleted `generated-icon.png` and `replit.md` files.
    - Reformatted `next-i18next.config.js`, `postcss.config.mjs`, `package.json`, and `tailwind.config.js` for consistent indentation.
    - Updated `package-lock.json` and `tsconfig.json` for improved structure.
    - Enhanced `globals.css` and `reactflow-custom.css` for better styling consistency.

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'vi'],
-  },
-  reloadOnPrerender: process.env.NODE_ENV === 'development',
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en', 'vi'],
+    },
+    reloadOnPrerender: process.env.NODE_ENV === 'development',
 }
